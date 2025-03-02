921e9ae214916d76da0ed6f68ee930704b94a983
jmoneill15 <jmoneill@udel.edu>
2025-03-02 09:33:14 -0500

    small change

--- PandaProgress/app/index.tsx
+++ PandaProgress/app/index.tsx
@@ -240,7 +240,7 @@ const styles = StyleSheet.create({
     //justifyContent: "center",
     alignItems: "center",
     width: "100%",
-    height: "400%",
+    height: "370%",
     // marginTop: "5%",
     // backgroundColor: "red",
 
